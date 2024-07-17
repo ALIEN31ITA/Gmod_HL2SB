73eed5a6a4eb618ee088ad22c97a507aa4bd9104
eon (bloodycop) <minerva.servers.eon@gmail.com>
2024-07-18 00:24:59 +0300

    thing

--- lua/autorun/server/hl2sb_init.lua
+++ lua/autorun/server/hl2sb_init.lua
@@ -18,13 +18,23 @@ if ( IsMounted( "ep2" ) ) then
 	game.AddParticles( "particles/magnusson_burner.pcf" )
 end
 
-CreateConVar("hl2sb_npcgodmode", 0, {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Enable/Disable NPC Godmode")
+CreateConVar("hl2sb_npcgodmode", "1", {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Enable/Disable NPC Godmode for Story NPCs")
+
+local godModeNPCs = {
+	["alyx_name"] = true,
+	["barney_name"] = true,
+}
 
 hook.Add("EntityTakeDamage", "HL2SB_NPCGodmode", function(ent, dmginfo)
 	if ( !IsValid( ent ) ) then return end
 
 	local bGodmode = GetConVar("hl2sb_npcgodmode"):GetBool()
-	if ( ent:IsNPC() and bGodmode ) then
+	local eName = ent:GetName() or ""
+
+	local map = game.GetMap()
+	if ( !map:find("gmhl2") ) then return end
+
+	if ( ent:IsNPC() and godModeNPCs[eName] and bGodmode ) then
 		return true
 	end
 end)
