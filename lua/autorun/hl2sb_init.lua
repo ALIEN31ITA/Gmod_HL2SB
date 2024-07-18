21517f2a047214ab9bda9fec115fe8595ae4e21c
eon (bloodycop) <minerva.servers.eon@gmail.com>
2024-07-18 04:38:22 +0300

    GlowLib support for vort, and W.I.P Advisor

--- lua/autorun/hl2sb_init.lua
+++ lua/autorun/hl2sb_init.lua
@@ -17,3 +17,38 @@ if ( IsMounted( "ep2" ) ) then
 	game.AddParticles( "particles/choreo_launch.pcf" )
 	game.AddParticles( "particles/magnusson_burner.pcf" )
 end
+
+if ( GlowLib ) then
+	GlowLib.Glow_Data["models/hl2sb/characters/vortigaunt_blue.mdl"] = table.Copy(GlowLib.Glow_Data["models/vortigaunt.mdl"])
+	local glowData = GlowLib.Glow_Data["models/hl2sb/characters/vortigaunt_blue.mdl"]
+
+	function glowData:ShouldDraw(ent, sprite)
+		local mat = Material("models/vortigaunt/vortigaunt_blue_ep1")
+		if ( !mat ) then return false end
+
+		local bEnabled = mat:GetFloat("$warpparam")
+		if ( bEnabled == 0 ) then return false end
+
+		return true
+	end
+
+	glowData.Color = {
+		[0] = Color(0, 120, 255)
+	}
+
+	glowData.Size = 0.5
+
+	GlowLib:Define("models/hl2sb/characters/advisor_ep2.mdl", {
+		Position = function(self, ent)
+			if ( !IsValid(ent) ) then return end
+
+			local attachmentData = ent:GetAttachment(ent:LookupAttachment("attach_mask_end_l"))
+			return attachmentData.Pos
+		end,
+		Attachment = "attach_mask_end_l",
+		Color = {
+			[0] = Color(0, 255, 255)
+		},
+		Size = 0.35
+	})
+end
