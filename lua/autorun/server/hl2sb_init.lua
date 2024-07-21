314f8419e8bfac9fd647158f0fb6e4a3ca960d73
eon (bloodycop) <minerva.servers.eon@gmail.com>
2024-07-21 23:50:39 +0300

    Update hl2sb_init.lua

--- lua/autorun/server/hl2sb_init.lua
+++ lua/autorun/server/hl2sb_init.lua
@@ -1,21 +1,26 @@
 CreateConVar("hl2sb_npcgodmode", "1", {FCVAR_ARCHIVE, FCVAR_NOTIFY}, "Enable/Disable NPC Godmode for Story NPCs")
 
-local godModeNPCs = {
+local godModeNPCNames = {
 	["alyx_name"] = true,
 	["barney_name"] = true,
+}
+
+local godModeNPCs = {
+	["npc_alyx"] = true,
+	["npc_barney"] = true,
 }
 
 hook.Add("EntityTakeDamage", "HL2SB_NPCGodmode", function(ent, dmginfo)
 	if ( !IsValid( ent ) ) then return end
 
 	local bGodmode = GetConVar("hl2sb_npcgodmode"):GetBool()
-	local eName = ent:GetName() or ""
+	local ent_class = ent:GetClass():lower()
+	local ent_name = ent:GetName():lower()
 
 	local map = game.GetMap()
 	if ( !map:find("gmhl2") ) then print("wrong map") return end
 
-	print(godModeNPCs[eName:lower()], "name match")
-	if ( ent:IsNPC() and godModeNPCs[eName:lower()] and bGodmode ) then
+	if ( ent:IsNPC() and ( godModeNPCs[ent_class] and godModeNPCNames[ent_name] ) and bGodmode ) then
 		return true
 	end
 end)
