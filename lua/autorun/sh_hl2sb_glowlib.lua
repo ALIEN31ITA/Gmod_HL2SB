c04cca551b6937c2456bd5449b3e2643a998d123
eon (bloodycop) <minerva.servers.eon@gmail.com>
2024-08-01 18:42:00 +0300

    Update sh_hl2sb_glowlib.lua

--- lua/autorun/sh_hl2sb_glowlib.lua
+++ lua/autorun/sh_hl2sb_glowlib.lua
@@ -2,25 +2,6 @@ if ( !GlowLib ) then
     return MsgC(Color(255, 0, 0), "[ GlowLib ] ", color_white, "GlowLib is not installed! You could install it to use the custom glow data for HL2 Sandbox Support!\n")
 end
 
-GlowLib.Glow_Data["models/hl2sb/characters/vortigaunt_blue.mdl"] = table.Copy(GlowLib.Glow_Data["models/vortigaunt.mdl"])
-local glowData = GlowLib.Glow_Data["models/hl2sb/characters/vortigaunt_blue.mdl"]
-
-function glowData:ShouldDraw(ent, sprite)
-    local mat = Material("models/vortigaunt/vortigaunt_blue_ep1")
-    if ( !mat ) then return false end
-
-    local bEnabled = mat:GetFloat("$warpparam")
-    if ( bEnabled == 0 ) then return false end
-
-    return true
-end
-
-glowData.Color = {
-    [0] = Color(0, 120, 255)
-}
-
-glowData.Size = 0.5
-
 GlowLib:Define("models/hl2sb/characters/advisor_ep2.mdl", {
     Position = function(self, ent)
         if ( !IsValid(ent) ) then return end
