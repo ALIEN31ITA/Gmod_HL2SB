a973a1c5b25c4a549a48ac3966494f6daf028ad5
eon (bloodycop) <minerva.servers.eon@gmail.com>
2024-08-01 17:47:58 +0300

    optimize

--- lua/autorun/server/hl2sb_init.lua
+++ lua/autorun/server/hl2sb_init.lua
@@ -16,20 +16,18 @@ hook.Add("EntityTakeDamage", "HL2SB_NPCGodmode", function(ent, dmginfo)
 	if ( !IsValid( ent ) ) then return end
 
 	local bGodmode = GetConVar("hl2sb_npcgodmode"):GetBool()
-	local ent_class = ent:GetClass():lower()
 	local ent_name = ent:GetName()
 
 	local map = game.GetMap()
 	if ( !map:find("gmhl2") ) then return end
 
+	local godModeTable = godModeNPCNames[ent_class]
+	if ( !godModeTable ) then return end
+
 	if ( ent:IsNPC() and bGodmode ) then
-		for k, v in pairs(godModeNPCNames) do
-			if ( ent_class == k ) then
-				for index, entName in ipairs(v) do
-					if ( ent_name == entName ) then
-						return true
-					end
-				end
+		for k, v in ipairs(godModeTable) do
+			if ( ent_name == entName ) then
+				return true
 			end
 		end
 	end
