51596da33cd16b41214c62ba3197ec3fa059d49b
eon (bloodycop) <minerva.servers.eon@gmail.com>
2024-08-02 20:48:16 +0300

    Update cl_hl2_episodiccredits.lua

--- lua/autorun/client/cl_hl2_episodiccredits.lua
+++ lua/autorun/client/cl_hl2_episodiccredits.lua
@@ -22,43 +22,38 @@ surface.CreateFont("HL2SBGenericFont", {
     shadow = false
 })
 
-local fadeStart = 0
-local fadeEnd = 5
-local fadeHold = 3
-local fadeTime = fadeEnd - fadeStart
+local HL2SB_IntroFadeStart = 0
+local HL2SB_IntroFadeEnd = 5
+local HL2SB_IntroFadeHold = 3
+local HL2SB_IntroFadeTime = HL2SB_IntroFadeEnd - HL2SB_IntroFadeStart
 
 concommand.Add("HL2SB_StartIntro", function()
-    fadeStart = CurTime()
-    fadeEnd = CurTime() + fadeTime
-    fadeTime = fadeEnd - fadeStart
-end)
-
-concommand.Add("HL2SB_DisplayCredits", function()
-    if ( IsValid(hl2credits_panel) ) then
-        hl2credits_panel:Remove()
-        hl2credits_panel = nil
-    end
-
-    hl2credits_panel = vgui.Create( "DPanel" )
-    hl2credits_panel:SetPos( 0, 0 )
-    hl2credits_panel:SetSize( ScrW(), ScrH() )
-    hl2credits_panel:SetBackgroundColor(ColorAlpha(color_white, 0))
-
-    local Avatar_Bloodycop = vgui.Create("AvatarImage", Panel)
-    Avatar_Bloodycop:SetSize(128, 128)
-    Avatar_Bloodycop:SetPos(ScrW() / 2 - 300, ScrH() / 2 - 128)
-    Avatar_Bloodycop:SetSteamID("76561198373309941") // eon ( bloodycop )
-
-    local Avatar_Alien31 = vgui.Create("AvatarImage", Panel)
-    Avatar_Alien31:SetSize(128, 128)
-    Avatar_Alien31:SetPos(ScrW() / 2 + 200, ScrH() / 2 - 128)
-    Avatar_Alien31:SetSteamID("76561198130175522") // Alien31
+    HL2SB_IntroFadeStart = CurTime()
+    HL2SB_IntroFadeEnd = CurTime() + HL2SB_IntroFadeTime
+    HL2SB_IntroFadeTime = HL2SB_IntroFadeEnd - HL2SB_IntroFadeStart
 end)
 
 concommand.Add("HL2SB_StopIntro", function()
-    fadeStart = 0
-    fadeEnd = 5
-    fadeTime = fadeEnd - fadeStart
+    HL2SB_IntroFadeStart = 0
+    HL2SB_IntroFadeEnd = 5
+    HL2SB_IntroFadeTime = HL2SB_IntroFadeEnd - HL2SB_IntroFadeStart
+end)
+
+local HL2SB_OutroFadeStart = 0
+local HL2SB_OutroFadeEnd = 5
+local HL2SB_OutroFadeHold = 3
+local HL2SB_OutroFadeTime = HL2SB_OutroFadeEnd - HL2SB_OutroFadeStart
+
+concommand.Add("HL2SB_DisplayCredits", function()
+    HL2SB_OutroFadeStart = CurTime()
+    HL2SB_OutroFadeEnd = CurTime() + HL2SB_OutroFadeTime
+    HL2SB_OutroFadeTime = HL2SB_OutroFadeEnd - HL2SB_OutroFadeStart
+end)
+
+concommand.Add("HL2SB_StopCredits", function()
+    HL2SB_OutroFadeStart = 0
+    HL2SB_OutroFadeEnd = 5
+    HL2SB_OutroFadeTime = HL2SB_OutroFadeEnd - HL2SB_OutroFadeStart
 end)
 
 local creditsMap = {
@@ -68,24 +63,55 @@ local creditsMap = {
     ["ep2_outland_01"] = "HALF-LIFE'\n== episode two=="
 }
 
-hook.Add("HUDPaint", "HL2EP1SandboxCredits", function()
+local function DrawIntro()
     local curTime = CurTime()
     local alpha = 255
 
-    if ( fadeStart == 0 ) then return end
+    if ( HL2SB_OutroFadeStart == 0 ) then return end
+    if ( alpha == 0 ) then return end
 
-    if curTime < fadeEnd then
-        alpha = Lerp((curTime - fadeStart) / fadeTime, 0, 255)
-    elseif curTime > fadeEnd + fadeHold then
-        alpha = Lerp((curTime - fadeEnd - fadeHold) / fadeTime, 255, 0)
+    if curTime < HL2SB_IntroFadeEnd then
+        alpha = Lerp((curTime - HL2SB_IntroFadeStart) / HL2SB_IntroFadeTime, 0, 255)
+    elseif curTime > HL2SB_IntroFadeEnd + HL2SB_IntroFadeHold then
+        alpha = Lerp((curTime - HL2SB_IntroFadeEnd - HL2SB_IntroFadeHold) / HL2SB_IntroFadeTime, 255, 0)
     end
 
-	local hl2sb_markupfonts = markup.Parse("<font=HL2SBCreditsFont>HALF-LIFE'</font>\n<font=HL2SBGenericFont>== sandbox==</font>")
+	local hl2sb_markupfonts = markup.Parse("<font=HL2SBCreditsFont>HALF-LIFE'</font>\n<font=HL2SBGenericFont>   == sandbox==</font>")
+	local mapName = creditsMap[hl2credits_getmap]
+    if ( mapName ) then
+	    draw.DrawText(mapName, "HL2SBCreditsFont", ScrW() / 2, ScrH() / 2.08, Color(255, 255, 255, alpha), TEXT_ALIGN_CENTER)
+    else
+        hl2sb_markupfonts:Draw(ScrW() / 2, ScrH() / 2.08, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, alpha)
+    end
+end
 
-	local mapName = creditsMap[hl2credits_getmap] or "==NOT==\nHALF-LIFE'\nMAP"
-	draw.DrawText(mapName, "HL2SBCreditsFont", ScrW() / 2, ScrH() / 2.08, Color(255, 255, 255, alpha), TEXT_ALIGN_CENTER)
-end)
+local OutroIcon_Bloodycop = Material("hl2sbox/credits/bloodycop.jpg")
+local function DrawOutro()
+    local curTime = CurTime()
+    local alpha = 255
 
-hook.Add("HUDPaint", "HL2SBAddonCredits", function()
-	//todo lol
+    if ( HL2SB_OutroFadeStart == 0 ) then return end
+    if ( alpha == 0 ) then return end
+
+    if curTime < HL2SB_OutroFadeEnd then
+        alpha = Lerp((curTime - HL2SB_OutroFadeStart) / HL2SB_OutroFadeTime * 10, 0, 255)
+    elseif curTime > HL2SB_OutroFadeEnd + HL2SB_OutroFadeHold then
+        alpha = Lerp((curTime - HL2SB_OutroFadeEnd - HL2SB_OutroFadeHold) / HL2SB_OutroFadeTime, 255, 0)
+    end
+
+    // Bloodycop Credits
+    surface.SetDrawColor(ColorAlpha(color_white, alpha))
+    surface.SetMaterial(OutroIcon_Bloodycop)
+    surface.DrawTexturedRect(ScrW() / 2 - 256, ScrH() / 2 - 64, 128, 128)
+
+    local markupBloodycop = markup.Parse("<font=HL2SBGenericFont>eon\n<color=255,0,0>bloodycop</color></font>")
+    markupBloodycop:Draw(ScrW() / 2 - 190, ScrH() / 1.83, TEXT_ALIGN_CENTER, TEXT_ALIGN_TOP, alpha, TEXT_ALIGN_CENTER)
+
+    // Alien31 Credits
+    // todo: below
+end
+
+hook.Add("HUDPaint", "HL2EP1Sandbox", function()
+    DrawIntro()
+    DrawOutro()
 end)
